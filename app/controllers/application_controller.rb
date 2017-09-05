79c043b2cb4e56dbeb8c67c7860d138abcf7e96f
GARCIA YUJI <garciayuji@LAB-N1235.local>
2017-09-05 13:41:56 +0900

    dbからデータを取り出したい

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  class School < ActiveRecord::Base
+    self.table_name = "schools"
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
 class MainController < ApplicationController
   def index
 
-    @file = File.new
+    # @file = File.new
+  end
+
+  def each_page
+    @school = School.where(id: 1).where(id: 1)
+    p @school
   end
 
 end
